bee77784be0766b3b2a01f835593c20a9a04e240
rajesh <rajeshp248@gmail.com>
2018-01-18 18:29:43 +0530

    Abstract Factory Method exampl-2 description added

--- src/main/java/com/designpatterns/guide/abstractfactory/example2/demo/Example2Demo.java
+++ src/main/java/com/designpatterns/guide/abstractfactory/example2/demo/Example2Demo.java
@@ -2,6 +2,12 @@ package com.designpatterns.guide.abstractfactory.example2.demo;
 
 import com.designpatterns.guide.abstractfactory.example2.factories.MobileFactory;
 
+/**
+ * For simple applications that do not need to create families of objects
+  but want to separate and centralize object creation.
+
+ Os and Display two different type of objects(related to different families)
+ */
 public class Example2Demo {
 
     public static void main(String a[]) {
